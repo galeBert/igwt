c6d65102ef706325e2d6c081fbcb238f4b8ff8ca
galeBert <76025267+galeBert@users.noreply.github.com>
2024-01-17 16:54:48 +0700

    new

--- app/api/biteship/create-order/callback/route.ts
+++ app/api/biteship/create-order/callback/route.ts
@@ -38,89 +38,92 @@ export async function POST(req: Request, res: Response) {
       where("shipping_status.tracking_id", "==", data.courier_tracking_id)
     );
     const querySnapshot = await getDocs(q);
-    console.log({ querySnapshot: querySnapshot });
-    console.log("for each", {
-      querySnapshot: querySnapshot.forEach((doc) => {
+    console.log({ querySnapshot: querySnapshot.docs });
+    if (!querySnapshot.empty) {
+      console.log("for each", {
+        querySnapshot: querySnapshot.forEach((doc) => {
+          transaction = { fbaseId: doc.id, ...doc.data() };
+        }),
+      });
+
+      querySnapshot.forEach((doc) => {
         transaction = { fbaseId: doc.id, ...doc.data() };
-      }),
-    });
-
-    querySnapshot.forEach((doc) => {
-      transaction = { fbaseId: doc.id, ...doc.data() };
-    });
-    const index = orderTrack.data.history.length
-      ? orderTrack.data.history.length - 1
-      : 0;
-    const docRef = doc(db, "transactions", transaction.fbaseId);
-    const status = orderTrack.data.status.split("_").join(" ");
-    console.log("body", {
-      "shipping_status.history": orderTrack.data.history.map(
-        (historyList: any) => ({
-          ...historyList,
-          status: historyList.status.split("_").join(" "),
-        })
-      ),
-      status:
-        status === "allocated"
-          ? "003"
-          : status === "picking up"
-          ? "114"
-          : status === "picked"
-          ? "214"
-          : status === "dropping off"
-          ? "314"
-          : status === "delivered"
-          ? "004"
-          : "",
-      transaction_status:
-        status === "delivered"
-          ? {
-              status: "DONE",
-              expired_at: new Date(
-                new Date().getTime() + 60 * 60 * 1000
-              ).toISOString(),
-            }
-          : undefined,
-    });
-
-    await updateDoc(docRef, {
-      "shipping_status.history": orderTrack.data.history.map(
-        (historyList: any) => ({
-          ...historyList,
-          status: historyList.status.split("_").join(" "),
-        })
-      ),
-      status:
-        status === "allocated"
-          ? "003"
-          : status === "picking up"
-          ? "114"
-          : status === "picked"
-          ? "214"
-          : status === "dropping off"
-          ? "314"
-          : status === "delivered"
-          ? "004"
-          : "",
-      transaction_status: {
-        status: status === "delivered" ? "DONE" : "ONPROGRESS",
-        expired_at:
+      });
+      const index = orderTrack.data.history.length
+        ? orderTrack.data.history.length - 1
+        : 0;
+      const docRef = doc(db, "transactions", transaction.fbaseId);
+      const status = orderTrack.data.status.split("_").join(" ");
+      console.log("body", {
+        "shipping_status.history": orderTrack.data.history.map(
+          (historyList: any) => ({
+            ...historyList,
+            status: historyList.status.split("_").join(" "),
+          })
+        ),
+        status:
+          status === "allocated"
+            ? "003"
+            : status === "picking up"
+            ? "114"
+            : status === "picked"
+            ? "214"
+            : status === "dropping off"
+            ? "314"
+            : status === "delivered"
+            ? "004"
+            : "",
+        transaction_status:
           status === "delivered"
-            ? new Date(new Date().getTime() + 60 * 60 * 1000).toISOString()
-            : null,
-      },
-    });
+            ? {
+                status: "DONE",
+                expired_at: new Date(
+                  new Date().getTime() + 60 * 60 * 1000
+                ).toISOString(),
+              }
+            : undefined,
+      });
 
-    const transactionData = await axios.post(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/transaction/${transaction.fbaseId}/transaction-log`,
-      {
-        role: orderTrack.data.courier.company,
-        description: orderTrack.data.history[index].note.split("_").join(" "),
-        status: orderTrack.data.status.split("_").join(" "),
-      }
-    );
+      await updateDoc(docRef, {
+        "shipping_status.history": orderTrack.data.history.map(
+          (historyList: any) => ({
+            ...historyList,
+            status: historyList.status.split("_").join(" "),
+          })
+        ),
+        status:
+          status === "allocated"
+            ? "003"
+            : status === "picking up"
+            ? "114"
+            : status === "picked"
+            ? "214"
+            : status === "dropping off"
+            ? "314"
+            : status === "delivered"
+            ? "004"
+            : "",
+        transaction_status: {
+          status: status === "delivered" ? "DONE" : "ONPROGRESS",
+          expired_at:
+            status === "delivered"
+              ? new Date(new Date().getTime() + 60 * 60 * 1000).toISOString()
+              : null,
+        },
+      });
 
-    return NextResponse.json(transactionData);
+      const transactionData = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/transaction/${transaction.fbaseId}/transaction-log`,
+        {
+          role: orderTrack.data.courier.company,
+          description: orderTrack.data.history[index].note.split("_").join(" "),
+          status: orderTrack.data.status.split("_").join(" "),
+        }
+      );
+
+      return NextResponse.json(transactionData);
+    }
+    return "not found";
   } catch (error) {
     console.log("error", error);
 
