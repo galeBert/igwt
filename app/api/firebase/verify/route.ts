fc4c2553aa007008d94b41334121ed1ed70077b8
galeBert <76025267+galeBert@users.noreply.github.com>
2024-01-31 14:22:20 +0700

    check new api

--- app/api/firebase/verify/route.ts
+++ app/api/firebase/verify/route.ts
@@ -10,37 +10,30 @@ import { db } from "@/lib/firebase";
 export async function POST(req: Request, res: NextApiResponse) {
   try {
     const header = headers().get("authorization");
+    console.log({ header });
+
     if (header) {
       const admin = initializeFirebaseAdmin();
 
       const token = header.split(" ")[1];
 
-      admin
-        .auth()
-        .verifyIdToken(token)
-        .then((decodedToken: any) => {
-          clerkClient.users
-            .createUser({
-              emailAddress: [decodedToken.email ?? ""],
-            })
-            .then(async (dataclerk) => {
-              const { id } = dataclerk;
+      const decodedToken = await admin.auth().verifyIdToken(token);
 
-              const transactionRef = doc(db, "user", id);
-              console.log("clerk jalan", dataclerk);
+      const clerkUser = await clerkClient.users.createUser({
+        emailAddress: [decodedToken.email ?? ""],
+      });
+      const { id } = clerkUser;
 
-              await setDoc(transactionRef, {
-                email: decodedToken.email,
-              });
-            })
-            .catch((error) => console.log("clerk error", error));
-        })
-        .catch((error: any) => {
-          console.log("asdsaddsa", error);
-        });
+      const transactionRef = doc(db, "user", id);
+
+      await setDoc(transactionRef, {
+        email: decodedToken.email,
+      });
+
+      return NextResponse.json({ email: decodedToken.email });
+    } else {
+      throw Error("Unauthorized");
     }
-
-    return NextResponse.json(header);
   } catch (error) {
     console.log(error);
 
