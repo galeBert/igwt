091b209a4d8325c268803344cfa6073f689b2eb9
galeBert <76025267+galeBert@users.noreply.github.com>
2024-02-11 21:36:10 +0700

    resend done

--- app/api/create-transaction-email/route.ts
+++ app/api/create-transaction-email/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-export const maxDuration = 300;
+
 export async function POST(req: Request, res: NextApiResponse) {
   try {
     const body = await req.json();
